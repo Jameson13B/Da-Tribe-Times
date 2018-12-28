8b6b8201aee73657382deb2aec3ba01f55c56dbd
Jameson13B <jamesonbrown@neolivity.com>
2018-12-28 14:16:36 -0700

    Removed bootstrapped data and added temp data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+`
+const Header = styled.header`
+  background-color: #572900;
+  min-height: 3rem;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: white;
+`
+const Title = styled.h1`
+  font-size: 1.5rem;
+`
+
+const Body = styled.div`
+  max-width: 768px;
+  margin: 0 auto;
+  padding: 15px 0;
+  text-align: center;
+`
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Container className="App">
+        <Header className="App-header">
+          <Title>Da Tribe Times</Title>
+        </Header>
+        <Body>
+          <h1 style={{ fontSize: "1rem" }}>More Coming Soon!</h1>
+          <br />
+          <p>Da Tribe Times provides a platform to list events and details for people to gather and enjoy good people,
+            good vibes, good music, and good food. Please visit us frequently to stay up to date and join us!</p>
+        </Body>
+      </Container>
     );
   }
 }
