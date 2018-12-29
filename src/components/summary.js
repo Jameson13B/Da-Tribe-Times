c1c0c7da17c7e1e189dd9cd04be5f79a454e3690
Jameson13B <jamesonbrown@neolivity.com>
2018-12-28 19:39:01 -0700

    Added link around summary and formatted date

--- src/components/summary.js
+++ src/components/summary.js
@@ -1,5 +1,7 @@
 import React from "react";
 import styled from "styled-components";
+import moment from "moment";
+import { Link } from "react-router-dom";
 
 const Container = styled.div`
   background: #F2D2A9;
@@ -10,6 +12,10 @@ const Container = styled.div`
   padding: 15px;
   text-align: start;
   width 500px;
+`;
+const NewLink = styled(Link)`
+  color: black;
+  text-decoration: none;
 `;
 const Title = styled.h1`
   font-size: 1.5rem;
@@ -30,10 +36,12 @@ const Sub = styled.h1`
 const Summary = props => {
   return (
     <Container className="Summary">
-      <Title>{props.event.title}</Title>
-      <Sub>Date of Event</Sub>
-      <Sub>{props.event.location}</Sub>
-      <Sub>{props.event.creator}</Sub>
+      <NewLink to={`/event/${props.index}`}>
+        <Title>{props.event.title}</Title>
+        <Sub>{moment(props.event.date).format("ddd M/D/YYYY - h:mm a")}</Sub>
+        <Sub>{props.event.location}</Sub>
+        <Sub>{props.event.creator}</Sub>
+      </NewLink>
     </Container>
   );
 };
